52245ecb6f92a738231829843d96640701e2968e
David Ferraz <davidferrazp@gmail.com>
2023-10-10 22:03:33 -0300

    Implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,4 +1,39 @@
-#!/c/Users/David/anaconda3/env python
+#!/usr/bin/env python
+"""
+Hello World Multi Linguas.
 
-#Imprime Hello World
-print('Hello, World!')
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ouenv pytho
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "David Ferraz"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg)
